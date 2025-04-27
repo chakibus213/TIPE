7d6d7a42af89b6dd6a535fae00d0a32c96f885a6
chakibus213 <chakib.boukheloua@hotmail.fr>
2025-04-27 22:28:29 +0200

    Add files via upload

--- jeu_v2.c
+++ jeu_v2.c
@@ -1,21 +1,24 @@
-/*
-
-
-- processus stochastique pour avoir une évaluation fiable de la stratégie
-- implémenter la stratégie de las vegas
-
-
-*/
-
-
-
-
+// Online C compiler to run C program online
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <assert.h>
+
+
+// regarder dans le cas ou on remet les cartes dans le paquet et quand on les remet pas : c'est codé tout est ok
+// 1 er cas : bienaymé tcébitchev
+// 2ème cas : incertitude type avec méthode de monte-carlo pour évaluer la fiabilité 
+
+//refaire le dénombrement : pas loin 
+
+
+
+
+// analyser la stratégie de seuil et son resultat : ok 
+
+
 
 //1-13 carreau  
 //14-26 coeur
@@ -24,95 +27,98 @@
 
 //2:10 chiffres 1:as 11:valet 12:dame 13:roi
 
+int denombrement(int cartes[53],int* score_joueur, int* score_croupier)
+{
+    int* compte_score = malloc (11*sizeof(int)) ;
+    float nb_cartes = 0;
+    float p_depass = 0.0;
+    for (int i=1; i<=11; i+=1)
+    {
+        compte_score[i] = 0;
+    }
+   
 
-//rep_user : 0:reste 1:tire 2:split 4:doublé
+    
+    if(*score_joueur <= 10)  //sur de pas dépasser
+    {
+
+    for (int i=1; i<=52; i+=1)
+    {
+        if(cartes[i] > 0)
+        {
+        nb_cartes+=1.0;
+        if((i-1)%13 == 0)
+            compte_score[11]+=1;
+
+        if((i-1)%13 >= 2 && (i-1)%13 <=10)
+        {
+            compte_score[(i-1)%13] +=1;
+        }
+
+        if( (i-1)%13 > 10 )
+        {
+            compte_score[10] +=1 ;
+        }
+        }
+    
+    return 1;
+        
+    }
+
+
+    }
+
+     if(*score_joueur > 10) //peut dépasser
+    {
+
+    for (int i=1; i<=52; i+=1)
+    {
+        if(cartes[i] > 0)
+        {
+        nb_cartes +=1.0;
+        if((i-1)%13 == 0)
+            compte_score[1]+=1;
+
+        if((i-1)%13 >= 2 && (i-1)%13 <=10)
+        {
+            compte_score[(i-1)%13] +=1;
+        }
+
+        if( (i-1)%13 > 10 )
+        {
+            compte_score[10] +=1 ;
+        }
+        }
+    }
+    for(int i=21; i <= 11  ; i += 1)
+    {
+        p_depass += compte_score[i] / nb_cartes ;
+    }
+
+    if(p_depass >= 0.3 ) 
+        return 0;
+    }
+
+   else
+    {
+        return 1;
+    }
+
+   
+
+}
+
+
+
+
 
 int heuristique1(int score_joueur) //s'arrêter après un score
 {
-    if(score_joueur <= 11)
+    if(score_joueur <= 21)
         return 1;
     else
         return 0;
 }
-
- 
- 
- 
- 
-
-int strategie_brute (int score, int cartes[53])
-{
-    int aug[12]; //de 1 à 11 le 0 est inutile
-    for(int i=1 ;i<=11; i++)
-        aug[i]=0;
-    
-    if(score<11)
-    {
-        for(int i=1; i<=52; i++) //on parcourt les cartes 
-        {
-            if(cartes[i] != 0) //verifie si elle est encore la
-            {
-                if((i-1)%13 == 10 || (i-1)%13 == 11 || (i-1)%13 == 12)
-                {
-                    aug[10]+=1;
-                }
-                
-                else if( (i-1)%13==0 )
-                    aug[11] += 1;
-                    
-                else
-                    aug[(i-1)%13+1] += 1;
-            }
-                
-        }
-    }
-    
-    else
-    {
-        for(int i=1; i<=52; i++) //on parcourt les cartes 
-        {
-            if(cartes[i] != 0) //verifie si elle est encore la
-            {
-                if((i-1)%13 == 10 || (i-1)%13 == 11 || (i-1)%13 == 12)
-                {
-                    aug[10]+=1;
-                }
-                
-                else if( (i-1)%13==0 )
-                    aug[1] += 1;
-                    
-                else
-                    aug[(i-1)%13+1] += 1;
-            }
-                
-        }
-    }
-    
-    int max_ind = 1;
-    float card_univers=0.0; //le nombre de tirages possibles (en gros le nombre de cartes restantes)
-    float card_depasse = 0.0; //le nombre de tirages qui fait depasser 21
-    for(int i=1; i<=11; i++)
-    {
-        card_univers += aug[i];
-        
-        if(i+score>21)
-        {
-            card_depasse += aug[i];
-        }
-    }
-    
-    float proba_depasse = card_depasse/card_univers;
-    
-    if(proba_depasse >= 0.5)
-        return 0;
-    
-    else
-        return 1;
-}
-
-
-
-
 
 
 
@@ -141,8 +147,6 @@ int strategie_optimale(int main_croupier,int main_joueur)
 
         if(i>=4 && i<=11)
             return 1;
-
-
         else
             return 0;
 
@@ -224,7 +228,10 @@ void affiche_carte (int numero)
     int hauteur = (numero-1) % 13;
     int couleur = (numero-1)/13;
     
-    if(hauteur == 10)
+    if(hauteur <= 9 && hauteur >= 1)
+		printf("%d ", hauteur+1);
+    
+    else if(hauteur == 10)
 		printf("Valet");
 	
 	else if(hauteur == 11)
@@ -250,7 +257,7 @@ void affiche_carte (int numero)
 }
 
 
-void tirage_carte(int* cartes, int* score, int* nb_cartes, int m[10])
+void tirage_carte(int cartes[53], int* score)
 {
     int tire=0; //0 -> carte tjrs pas tiré 1-> carte tiré
     
@@ -268,9 +275,8 @@ void tirage_carte(int* cartes, int* score, int* nb_cartes, int m[10])
         else
         {
             tire=1;
-            m[*nb_cartes]=num_carte;
-            nb_cartes += 1 ;
-            cartes[num_carte] -= 1;
+            
+            cartes[num_carte] = 0;
         }
     }
     
@@ -297,133 +303,124 @@ void tirage_carte(int* cartes, int* score, int* nb_cartes, int m[10])
 	
 }
 
-
-int calcul_mise( int* gains, int* is_playing) //calcule la mise à mettre en fonction des gains
+int est_vide (int cartes[53])
 {
-    if (*gains>=20)
+    for(int i=0; i<53; i+=1)
     {
-        return 10;
-    }
-    else if(*gains >= 1)
-    {
-        return 1;
+        if(cartes[i]==1)
+        {
+            return 0;
+        }
     }
 
-    else
-    {
-        *is_playing =0;
-        return 0;
-    }
+    return 1;
 }
 
-int doublet(int* gains, int* score_joueur, int* mise) //determine si on double ou pas
-{
-    if(*gains >= 20 && *gains > 2* (*mise)  && (*score_joueur==10 || *score_joueur==11))
-    {
-        return 1;
-    }
 
-    else
+void tirage_carte_aux(int cartes[53], int* score) //tirage carte pour le cas ou on remet pas les cartes dans le paquet
+{
+    int tire=0; //0 -> carte tjrs pas tiré 1-> carte tiré
+    
+    //srand(time(NULL));
+    int num_carte= ceil(rand()%52 + 1);    
+
+    while (!tire)
     {
-        return 0;
+        
+        if(cartes[num_carte] == 0)
+        {
+            num_carte= rand()%52 + 1;
+            
+        }
+        
+        else
+        {
+            tire=1;
+            cartes[num_carte] -= 1;
+        }
     }
+    
+    if( (num_carte-1)%13 == 0)
+    {
+		if (*score + 11 > 21)
+		{
+			*score = *score +1;
+		}
+		
+		else
+		{
+			*score += 11;
+		}			
+	}
+	
+	if( (num_carte-1)%13 >= 1 && (num_carte-1)%13 <= 9  )
+    {
+		*score += (num_carte-1)%13 + 1;
+	}
+	
+	else
+		*score += 10;
+
+
+
 }
 
-int deck_jouable(int* nb_cartes)
-{
-    int nb_c=0;
-    for(int i=1; i<=52;i+=1)
-    {
-       nb_c += 1;
-    }
 
-    if(nb_c >=20)
-        return 1;
-    else
-        return 0;
-}
+
+
 
 int main() 
 {
+    
     srand(time(NULL));
 
-    int *is_playing= malloc(sizeof(int));
-    
-    *is_playing = 1;
-    
-    int* gains = malloc(sizeof(int));
-    *gains=100;
-    int* is_split = malloc(sizeof(int));
-    int* is_double = malloc(sizeof(int));
-    
-    int* mise = malloc(sizeof(int));
-    *mise = 0;
-    int main_joueur[10];
-    int main_croupier[6];
-    int* nb_cartes_j = malloc(sizeof(int));
-    int* nb_cartes_c= malloc(sizeof(int));
-    
+    int cartes [53];
     int* score_joueur= malloc(sizeof(int));
     int* score_croupier=malloc(sizeof(int));
-    
     int rep_user;
     int partie_en_cours;
     int joueur_en_cours;
-    int* cartes = malloc(52*sizeof(int));
-    for(int i=1; i<=52; i+=1)
-    {
-        cartes[i]=8;
-    }
     
-    int carte_croupier;
+    int nb_d=0;
+    int nb_v=0;
+    int nb_n=0;
+    float nb_parties = 500000.0;
+    int nb_dd=0; //défaiteses par dépassement
+    int nb_ds=0; //défaites par score
+    int nb_vd=0; //victoires par dépassement
+    int nb_vs=0; //victoires par score
 
-
-    int c=0;
-    *mise = calcul_mise(gains, is_playing);
-    while ( c<30 && is_playing && deck_jouable(cartes) == 0)    
+    for(int c=0; c<nb_parties; c++)    //inéalité de bienaymé tchébytchev pour trouver le bon nombre de tentatives/ cas ou on remet les cartes dans le paquet
     {
-        
-        
-        is_double=0;
-        is_split=0;
-		*nb_cartes_j=0;
-		*nb_cartes_c=0;
-		for (int i=1; i<10; i++)
-        {
-			main_croupier[i]=0;
-			main_joueur[i]=0;
-		}
-		
-		carte_croupier=0;
         *score_joueur=0;
         *score_croupier=0;
+        cartes[0]=0;
+        for (int i=1; i<53; i++)
+        {
+            cartes[i]=i;
+        }
         
         partie_en_cours=1; // 1-> partie pas terminé 0-> partie terminé
         
-        joueur_en_cours=1; //1->joueur 0->croupier
+        joueur_en_cours=1;
 
            
-           tirage_carte(cartes, score_joueur, nb_cartes_j,main_joueur); //tire les deux premiere carte
-           tirage_carte(cartes, score_joueur, nb_cartes_j, main_joueur); 
+           tirage_carte(cartes, score_joueur); //tire les deux premiere carte
+           tirage_carte(cartes, score_joueur); 
            
-           tirage_carte(cartes, score_croupier, nb_cartes_c, main_croupier);
+           tirage_carte(cartes, score_croupier); 
            
-           
+        
+
            while(joueur_en_cours == 1 && partie_en_cours ==1)
            {
-			   rep_user = strategie_optimale(*score_croupier, *score_joueur);
+			   //rep_user = strategie_optimale(*score_croupier, *score_joueur);
 			   //rep_user = decision_aleatoire();
-			   //rep_user = strategie_brute(*score_joueur, cartes);
-			  //rep_user = heuristique1(*score_joueur);
-               //rep_user=0;
-               if(doublet(gains, score_joueur, mise) == 1)
-               {
-                *mise = *mise * 2;
-               }
-
+			   //rep_user = heuristique1(*score_joueur);
+               rep_user = denombrement(cartes, score_joueur, score_croupier);
                if(rep_user == 1)
 			   {
-				tirage_carte(cartes, score_joueur, nb_cartes_j, main_joueur);
+				tirage_carte(cartes, score_joueur);
 				if((*score_joueur)>21)
                     partie_en_cours = 0;
                }
@@ -432,37 +429,119 @@ int main()
                 joueur_en_cours = 0;
            }
            
+          
            while(*score_croupier < 17 && partie_en_cours == 1)
            {
-			   
-               tirage_carte(cartes, score_croupier, nb_cartes_c, main_croupier);
+               tirage_carte(cartes, score_croupier);
+              
                if (*score_croupier > 21)
                     partie_en_cours = 0;
            }
            
            if(*score_croupier > 21 || ( *score_croupier < *score_joueur && *score_joueur <=21) )
            {
-                 *gains += *mise ;
-                 *mise = calcul_mise(gains, is_playing);
-                  c+=1;
+             nb_v+=1;
            }
            
            else if (*score_joueur > 21 || (*score_croupier > *score_joueur && *score_croupier <= 21) )
            {
-             *gains -= *mise ;
-             *mise = calcul_mise(gains, is_playing);
-             c+=1;
+             nb_d +=1;
            }
            
            else if (*score_joueur == *score_croupier)
            {
-              *mise = calcul_mise(gains, is_playing);
-              c+=1;
+              nb_n +=1;
            }
     }    
     
     
-    printf("Resultat d'Ilian : Gains totaux: %d", *gains);
+    /*
+    for (int i=1; i<53; i++)
+        {
+            cartes[i]=10000;
+        }
+
+    for(int c=0; c<nb_parties; c++)    //utiliser la méthode de monte-carlo pour calculer la bonne valeur 
+    {
+        *score_joueur=0;
+        *score_croupier=0;
+        
+        partie_en_cours=1; // 1-> partie pas terminé 0-> partie terminé
+        
+        joueur_en_cours=1;
+
+       
+           tirage_carte_aux(cartes, score_joueur); //tire les deux premiere carte
+           tirage_carte_aux(cartes, score_joueur); 
+           
+           tirage_carte_aux(cartes, score_croupier); 
+           
+        
+
+           while(joueur_en_cours == 1 && partie_en_cours ==1)
+           {
+			   //rep_user = strategie_optimale(*score_croupier, *score_joueur);
+			   //rep_user = decision_aleatoire();
+			   rep_user = heuristique1(*score_joueur);
+               //rep_user = denombrement(cartes, score_joueur, score_croupier);
+               if(rep_user == 1)
+			   {
+				tirage_carte_aux(cartes, score_joueur);
+				if((*score_joueur)>21)
+                    partie_en_cours = 0;
+               }
+               
+               else
+                joueur_en_cours = 0;
+           }
+           
+           
+           
+           while(*score_croupier < 17 && partie_en_cours == 1)
+           {
+               tirage_carte_aux(cartes, score_croupier);
+              
+               if (*score_croupier > 21)
+                    partie_en_cours = 0;
+           }
+           
+           
+
+           if(*score_croupier > 21)
+           {
+             nb_v+=1;
+             nb_vd +=1;
+           }
+
+           if(*score_croupier < *score_joueur && *score_joueur <=21)
+           {
+            nb_v += 1;
+            nb_vs += 1;
+           }
+           
+           else if (*score_joueur > 21 )
+           {
+             nb_d +=1;
+             nb_dd += 1;
+           }
+           
+           else if(*score_croupier > *score_joueur && *score_croupier <= 21)
+           {
+            nb_d +=1;
+            nb_ds += 1;
+           }
+
+           else if (*score_joueur == *score_croupier)
+           {
+              nb_n +=1;
+           }
+    }
+
+    */
+    
+   printf("Victoires : %f%, Nuls   : %f%, Défaites :%f% \n", nb_v/nb_parties*100, nb_n/nb_parties*100, nb_d/nb_parties*100);
+   /*
+   printf("Défaites par dépassement : %.2f%%, Défaites par score : %.2f%%\n", nb_dd * 100.0 / nb_d, nb_ds * 100.0 / nb_d);
+   printf("Victoires par dépassement : %.2f%%, Victoires par score : %.2f%%\n", nb_vd * 100.0 / nb_v, nb_vs * 100.0 / nb_v); */
    return EXIT_SUCCESS;
 }
-
